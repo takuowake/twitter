df6513ce22328a1d0792cf30072711487cd931f6
takuowake <takuowake@gmail.com>
2023-02-17 11:06:48 +0900

    タイムラインに投稿を表示

--- lib/utils/firestore/users.dart
+++ lib/utils/firestore/users.dart
@@ -60,6 +60,31 @@ class UserFirestore {
       print('ユーザー情報の更新エラー：$e');
       return false;
     }
+
+  }
+  static Future<Map<String, Account>?> getPostUserMap(List<String> accountIds) async {
+    Map<String, Account> map = {};
+    try {
+      await Future.forEach(accountIds, (String accountId) async {
+        var doc = await users.doc(accountId).get();
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        Account postAccount = Account(
+          id: accountId,
+          name: data['name'],
+          userId: data['user_id'],
+          imagePath: data['image_path'],
+          selfIntroduction: data['self_introduction'],
+          createdTime: data['created_time'],
+          updatedTime: data['updated_time']
+        );
+        map[accountId] = postAccount;
+      });
+      print('投稿ユーザー情報取得完了');
+      return map;
+    } on FirebaseException catch(e) {
+      print('投稿ユーザー情報取得エラー：$e');
+      return null;
+    }
   }
 }
 
--- lib/view/time_line/time_line_page.dart
+++ lib/view/time_line/time_line_page.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterlabo_twitter/model/account.dart';
 import 'package:flutterlabo_twitter/model/post.dart';
+import 'package:flutterlabo_twitter/utils/firestore/posts.dart';
+import 'package:flutterlabo_twitter/utils/firestore/users.dart';
 import 'package:flutterlabo_twitter/view/time_line/post_page.dart';
 import 'package:intl/intl.dart';
 
@@ -11,30 +13,6 @@ class TimeLinePage extends StatefulWidget {
 }
 
 class _TimeLinePageState extends State<TimeLinePage> {
-  Account myAccount = Account(
-    id: '1',
-    name: 'Flutterラボ',
-    selfIntroduction: 'こんばんは',
-    userId: 'flutter_labo',
-    imagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaVY7EbqchU2Yds9dk_aJZZI42ZW2dEuKc7f88TLIC7cTLKLr_BG4JXztbAY_Bk4-xBaw&usqp=CAU',
-    createdTime: Timestamp.now(),
-    updatedTime: Timestamp.now()
-  );
-
-  List<Post> postList = [
-    Post(
-    id: '1',
-    content: '初めまして',
-    postAccountId: '1',
-    createdTime: Timestamp.now()
-    ),
-    Post(
-        id: '2',
-        content: '初めまして2かい',
-        postAccountId: '1',
-        createdTime: Timestamp.now()
-    ),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,49 +22,82 @@ class _TimeLinePageState extends State<TimeLinePage> {
         backgroundColor: Theme.of(context).canvasColor,
         elevation: 2,
       ),
-      body: ListView.builder(
-          itemCount: postList.length,
-          itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                border: index == 0 ? Border(
-                  top: BorderSide(color: Colors.grey, width: 0),
-                  bottom: BorderSide(color: Colors.grey, width: 0),
-                ) : Border(bottom: BorderSide(color: Colors.grey, width: 0),)
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    radius: 22,
-                    foregroundImage: NetworkImage(myAccount.imagePath),
-                  ),
-                Expanded(
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: PostFirestore.posts.orderBy('created_time', descending: true).snapshots(),
+        builder: (context, postSnapshot) {
+          if(postSnapshot.hasData) {
+            List<String> postAccountIds = [];
+            postSnapshot.data!.docs.forEach((doc) {
+              Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+              if(!postAccountIds.contains(data['post_account_id'])) {
+                postAccountIds.add(data['post_account_id']);
+              }
+            });
+            return FutureBuilder<Map<String, Account>?>(
+              future: UserFirestore.getPostUserMap(postAccountIds),
+              builder: (context, userSnapshot) {
+                if(userSnapshot.hasData && userSnapshot.connectionState == ConnectionState.done) {
+                  return ListView.builder(
+                    itemCount: postSnapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      Map<String, dynamic> data = postSnapshot.data!.docs[index].data() as Map<String, dynamic>;
+                      Post post = Post(
+                        id: postSnapshot.data!.docs[index].id,
+                        content: data['content'],
+                        postAccountId: data['post_account_id'],
+                        createdTime: data['created_time']
+                      );
+                      Account postAccount = userSnapshot.data![post.postAccountId]!;
+                      return Container(
+                        decoration: BoxDecoration(
+                            border: index == 0 ? Border(
+                              top: BorderSide(color: Colors.grey, width: 0),
+                              bottom: BorderSide(color: Colors.grey, width: 0),
+                            ) : Border(bottom: BorderSide(color: Colors.grey, width: 0),)
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                        child: Row(
                           children: [
-                            Row(
-                              children: [
-                                Text(myAccount.name, style: TextStyle(fontWeight: FontWeight.bold),),
-                                Text('@${myAccount.userId}', style: TextStyle(color: Colors.grey),),
-                              ],
+                            CircleAvatar(
+                              radius: 22,
+                              foregroundImage: NetworkImage(postAccount.imagePath),
                             ),
-                            Text(DateFormat('M/d/yy').format(postList[index].createdTime!.toDate()))
+                            Expanded(
+                              child: Container(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text(postAccount.name, style: TextStyle(fontWeight: FontWeight.bold),),
+                                            Text('@${postAccount.userId}', style: TextStyle(color: Colors.grey),),
+                                          ],
+                                        ),
+                                        Text(DateFormat('M/d/yy').format(post.createdTime!.toDate()))
+                                      ],
+                                    ),
+                                    Text(post.content)
+                                  ],
+                                ),
+                              ),
+                            )
                           ],
                         ),
-                        Text(postList[index].content)
-                      ],
-                    ),
-                  ),
-                )
-                ],
-              ),
+                      );
+                    },
+                  );
+                } else {
+                  return Container();
+                }
+              }
             );
-          },
+          } else {
+            return Container();
+          }
+        }
       ),
     );
   }
